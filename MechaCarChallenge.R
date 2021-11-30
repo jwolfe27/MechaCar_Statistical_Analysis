8829651b9b939fc81e61287a0487c04456a9a510
Jake Wolfe <89044350+jwolfe27@users.noreply.github.com>
2021-11-30 18:11:40 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,9 @@ mechacar_mpg <- read.csv(file='MechaCar_mpg.csv', check.names = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg )
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg))
+
+mechacar_suspcoil <- read.csv(file = 'Suspension_Coil.csv', check.names = F)
+
+total_summary <- mechacar_suspcoil %>% summarize(mean_PSI=mean(PSI), median_PSI=median(PSI), var_PSI=var(PSI),st_dev_PSI=sd(PSI), .groups = 'keep')
+
+lot_summary <- mechacar_suspcoil %>% group_by(Manufacturing_Lot) %>% summarize(mean_PSI=mean(PSI), median_PSI=median(PSI), var_PSI=var(PSI), st_dev_PSI=sd(PSI), .groups='keep')
