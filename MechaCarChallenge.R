2c79ad230aab254836a69feff456a49de8c2b464
Jake Wolfe <89044350+jwolfe27@users.noreply.github.com>
2021-11-30 17:35:40 -0500

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,7 @@
+library(dplyr)
+
+mechacar_mpg <- read.csv(file='MechaCar_mpg.csv', check.names = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg )
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg))
