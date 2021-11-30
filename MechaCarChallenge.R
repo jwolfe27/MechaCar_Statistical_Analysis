9ce257a692e2723a86b6b1a098456d28c732252b
Jake Wolfe <89044350+jwolfe27@users.noreply.github.com>
2021-11-30 18:37:58 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,9 @@ mechacar_suspcoil <- read.csv(file = 'Suspension_Coil.csv', check.names = F)
 total_summary <- mechacar_suspcoil %>% summarize(mean_PSI=mean(PSI), median_PSI=median(PSI), var_PSI=var(PSI),st_dev_PSI=sd(PSI), .groups = 'keep')
 
 lot_summary <- mechacar_suspcoil %>% group_by(Manufacturing_Lot) %>% summarize(mean_PSI=mean(PSI), median_PSI=median(PSI), var_PSI=var(PSI), st_dev_PSI=sd(PSI), .groups='keep')
+
+t.test(mechacar_suspcoil$PSI, mu=1500)
+
+t.test(subset(mechacar_suspcoil$PSI, mechacar_suspcoil$Manufacturing_Lot=='Lot1'), mu=1500)
+t.test(subset(mechacar_suspcoil$PSI, mechacar_suspcoil$Manufacturing_Lot=='Lot2'), mu=1500)
+t.test(subset(mechacar_suspcoil$PSI, mechacar_suspcoil$Manufacturing_Lot=='Lot3'), mu=1500)
